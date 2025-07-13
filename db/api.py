46e82fc67146f47bd3d6b2fd12ced50e07eb0d19
AnishKamatam <anishkamatam22@gmail.com>
2025-07-13 11:23:03 -0700

    cors

--- db/api.py
+++ db/api.py
@@ -13,8 +13,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://viewlingo.vercel.app"
+        # "http://localhost:3000",
+        # "https://viewlingo.vercel.app"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
